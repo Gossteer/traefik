eb99e918172e900d0886d79d64c9c474915faaa8
Gossteer <GossteerPlus@gmail.com>
2021-09-02 11:10:15 +0300

    add mysql and portainer

---
+++ .docker/traefik/mysql/init/01-databases.sql
@@ -0,0 +1,5 @@
+# create databases
+CREATE DATABASE IF NOT EXISTS `dreamscometrue`;
+CREATE DATABASE IF NOT EXISTS `nailsmastersadmin`;
+CREATE DATABASE IF NOT EXISTS `nailsmasters`;
+CREATE DATABASE IF NOT EXISTS `maincard`;
